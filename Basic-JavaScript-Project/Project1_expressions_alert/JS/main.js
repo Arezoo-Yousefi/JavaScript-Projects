ade465ada6b0be25df338c4d99475cfba5a2f572
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-04-06 12:14:09 -0700

    some new javascript

---
+++ Basic-JavaScript-Project/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var b = "This is nothing!";
+window.alert(b);//this is an alert
+var a= "Hi, New World"
+document.write(a);//this write a in page;
+document.write('<h1>hello this is sentence with a \'.</h1>');
+var c = "This is simple concatenate";
+var d = " which is added to this sentence";
+document.write(c+d);
+var family= "my love", farther = "Ali", mother = "Arezoo", firstDaughter="Parmida", secondDaughter= "Mantra";
+document.write("<h3>The children are: "+firstDaughter+" and "+secondDaughter+"<h3>");
+
+var e = 2;
+var f = e +3;
+document.write(f); 
